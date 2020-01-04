05e025bf271763d9c2ce0af1fe04ea302b8e4205
Matias Roson <matias.roson@moni.com.ar>
2020-01-04 00:28:54 -0300

    Bug fixed celery beat with database scheduler

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,6 +1,4 @@
 """
-WSGI config for config project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
--- backend/run_task.sh
+++ backend/run_task.sh
@@ -2,9 +2,9 @@
 if [ "$SCHEDULER_MANAGER" = "static" ]
 then
 	celery -A config worker -l info --beat
-elif [ "$SCHEDULER_MANAGER" != "dynamic" ]
+elif [ "$SCHEDULER_MANAGER" = "dynamic" ]
 then
-	celery -A config worker -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+	celery -A config worker --beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 else
     celery -A config worker -l info
 fi
