d52b131575b6228cf1fbcfe20fecf0bc41deb38e
Matias <matiroson@gmail.com>
2022-03-29 16:45:23 -0300

    Create command to generate N random users

---
+++ apps/core/management/commands/generate_random_user.py
@@ -0,0 +1,28 @@
+import names
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = """
+    Create random users.
+    
+    For run this command need to have to "names" python dependency.
+    Intall it with "pip install names".
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('quantity', type=int, default=10)
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        for i in range(options['quantity']):
+            first_name, last_name = names.get_full_name().split(" ")
+
+            User.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                username=f"{first_name[0]}{last_name.lower()}",
+                email=f"{first_name.lower()}_{last_name.lower()}@mail.com"
+            )
