215c66bd42190a1180ea36770ac62be1a4e436f0
Matias <matiroson@gmail.com>
2022-05-18 10:36:23 -0300

    update readme, improvement command to generate random users, remove unnecessary definition on docker compose file and cosmetic change on home page

--- apps/core/management/commands/generate_random_user.py
+++ apps/core/management/commands/generate_random_user.py
@@ -1,6 +1,7 @@
 import names
 
 from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
 from django.contrib.auth import get_user_model
 
 
@@ -18,11 +19,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
         for i in range(options['quantity']):
-            first_name, last_name = names.get_full_name().split(" ")
+            try:
+                first_name, last_name = names.get_full_name().split(" ")
+            except ModuleNotFoundError:
+                print("Install names library with 'pip install names' before to run this command. ")
 
-            User.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                username=f"{first_name[0]}{last_name.lower()}",
-                email=f"{first_name.lower()}_{last_name.lower()}@mail.com"
-            )
+            try:
+                User.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    username=f"{first_name[0]}{last_name.lower()}",
+                    email=f"{first_name.lower()}_{last_name.lower()}@mail.com"
+                )
+            except IntegrityError:
+                pass
